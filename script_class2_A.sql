c1a682f6ef2d68b338196c5380feb50942075baf
AntManCan <a.canteiro@campus.fct.unl.pt>
2024-04-04 11:47:28 +0100

    backup
    Added View client_instalation

--- script_class2_A.sql
+++ script_class2_A.sql
@@ -18,7 +18,7 @@ values("2020-01-01", 12, 100, 1);
 INSERT INTO instalations(idInstalations, address, Client_NIF, Contracts_idContracts)
 values(123, 'Rua A', 999, 1);
     
-SELECT name, c.adress, idInstalations as instalation_code, i.address
+SELECT name, c.adress AS client_address, idInstalations as instalation_code, i.address AS instalation_address
 FROM client c
 INNER JOIN instalations i on c.NIF = i.Client_NIF
 ORDER BY
