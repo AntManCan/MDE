3b13f4358418781dbc6879edfe0a8f75153f3ce6
AntManCan <a.canteiro@campus.fct.unl.pt>
2024-04-10 17:22:21 +0100

    backup_procedures
    added devices to db, fixed typos
    added procedures and trigger

---
+++ SB_test.sql
@@ -0,0 +1,14 @@
+
+use smart_buildingsdb;
+
+call insert_client(999, 'Marcos', 'Rua 123, Lx', 999999999);
+
+call insert_instalation(123, 'Rua do Caralho 33', 999, 'casa');
+call insert_instalation(124, 'Rua do Hugo, 69', 999, 'fab');
+
+call insert_contract(123, "2021-01-01", "2025-01-01", 1000, 2);
+call insert_contract(124, "2022-01-01", "2026-01-01", 1200, 3);
+
+SELECT * FROM service_package;
+
+SELECT * FROM client_installations;
--- script_class2_A.sql
+++ script_class2_A.sql
@@ -8,19 +8,25 @@ values(999, 'Marcos', 'Rua 123, Lx', 999999999),
     (456, 'Rod', 'City 123, Lx', 999999992),
     (789, 'Jonh', 'Ass 123, Lx', 999999994),
     (321, 'Ggg', 'ugh 123, Lx', 999999993);
-    
+   
+-- delete from service_package where idService_Package=3;
 INSERT INTO service_package(idService_Package, name)
-values(1, 'Low');
+values
+	(2, 'High'),
+    (3, 'Pro');
     
 INSERT INTO contracts(start_date, duration, cost, Service_Package_idService_Package)
 values("2020-01-01", 12, 100, 1);
 
 INSERT INTO instalations(idInstalations, address, Client_NIF, Contracts_idContracts)
 values(123, 'Rua A', 999, 1);
-    
+
+CREATE OR REPLACE VIEW `client_installations` AS
 SELECT name, c.adress AS client_address, idInstalations as instalation_code, i.address AS instalation_address
 FROM client c
 INNER JOIN instalations i on c.NIF = i.Client_NIF
 ORDER BY
 	name ASC;
+    
+SELECT *FROM client_installations;
     
