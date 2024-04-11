cb1c774be8afd47aaf71fe48d9f836a43265c745
AntManCan <a.canteiro@campus.fct.unl.pt>
2024-04-11 20:05:50 +0100

    fixed bugs, added insert_device_info

--- SB_test.sql
+++ SB_test.sql
@@ -9,15 +9,10 @@ call insert_instalation(124, 'Rua do Hugo, 69', 999, 'fab');
 call insert_contract(123, "2021-01-01", "2025-01-01", 1000, 2);
 call insert_contract(124, "2022-01-01", "2026-01-01", 1200, 3);
 
-SELECT * FROM client_installations;
-
 call insert_receipt(1);
 
-call select_contract_pkg(1, @id, @name);
+call insert_device_info('M809', 'fridge', 'utility', 5);
+call install_device(123, 'M809', "2024-01-01");
 
-SELECT @id;
-SELECT @name;
 
-SELECT * FROM service_package;
 
-SELECT * FROM client_installations;
