5205bd09319c6270cb56adb2f0f6df30944028a6
AntManCan <a.canteiro@campus.fct.unl.pt>
2024-04-11 11:04:43 +0100

    added more procs

--- SB_test.sql
+++ SB_test.sql
@@ -9,6 +9,15 @@ call insert_instalation(124, 'Rua do Hugo, 69', 999, 'fab');
 call insert_contract(123, "2021-01-01", "2025-01-01", 1000, 2);
 call insert_contract(124, "2022-01-01", "2026-01-01", 1200, 3);
 
+SELECT * FROM client_installations;
+
+call insert_receipt(1);
+
+call select_contract_pkg(1, @id, @name);
+
+SELECT @id;
+SELECT @name;
+
 SELECT * FROM service_package;
 
 SELECT * FROM client_installations;
